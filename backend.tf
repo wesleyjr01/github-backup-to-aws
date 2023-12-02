125a23ca37005a6ce35c4aa5da9034f5340d72c7
Wesley Bortolozo <wesleyjr0101@gmail.com>
2023-12-02 12:37:25 -0300

    changed secret_arn and backend to my account

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region               = "us-east-1"
-    bucket               = "jheni-tfstate"
+    bucket               = "tfstate-wesley-011"
     key                  = "github-backup/state.tfstate"
   }
 }
